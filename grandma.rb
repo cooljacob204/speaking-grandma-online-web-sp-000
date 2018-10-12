47f3b9d80018b92016e1e0e312c2d05034608dcb
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-12 17:10:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,10 @@
 
 def speak_to_grandma(say)
   if(say == "I LOVE YOU GRANDMA!")
+    puts "I LOVE YOU TOO PUMPKIN!"
   elsif (say.upcase == say)
     puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
   end
 end
